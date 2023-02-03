27a21513c7657150d2259b97b7645981f9a3f281
Yashwant <yashwant8530@gmail.com>
2023-02-03 15:09:10 +0530

    fix edit form save button always loading

--- admin/client/App/screens/Item/components/EditForm.js
+++ admin/client/App/screens/Item/components/EditForm.js
@@ -1,37 +1,35 @@
-import React from 'react';
-import moment from 'moment';
-import assign from 'object-assign';
+import React from "react";
+import moment from "moment";
+import assign from "object-assign";
 import {
 	Form,
 	FormField,
 	FormInput,
 	Grid,
 	ResponsiveText,
-} from '../../../elemental';
+} from "../../../elemental";
 
-import { Fields } from 'FieldTypes';
-import { fade } from '../../../../utils/color';
-import theme from '../../../../theme';
+import { Fields } from "FieldTypes";
+import { fade } from "../../../../utils/color";
+import theme from "../../../../theme";
 
-import { Button, LoadingButton } from '../../../elemental';
-import AlertMessages from '../../../shared/AlertMessages';
-import ConfirmationDialog from '../../../shared/ConfirmationDialog';
+import { Button, LoadingButton } from "../../../elemental";
+import AlertMessages from "../../../shared/AlertMessages";
+import ConfirmationDialog from "../../../shared/ConfirmationDialog";
 
-import FormHeading from './FormHeading';
-import AltText from './AltText';
-import FooterBar from './FooterBar';
-import InvalidFieldType from '../../../shared/InvalidFieldType';
+import FormHeading from "./FormHeading";
+import AltText from "./AltText";
+import FooterBar from "./FooterBar";
+import InvalidFieldType from "../../../shared/InvalidFieldType";
 
-import { deleteItem } from '../actions';
+import { deleteItem } from "../actions";
 
-import { upcase } from '../../../../utils/string';
+import { upcase } from "../../../../utils/string";
 
-import { ASYNC_FIELD_LOADING, ASYNC_FIELD_LOADED } from '../constants';
-
-function getNameFromData (data) {
-	if (typeof data === 'object') {
-		if (typeof data.first === 'string' && typeof data.last === 'string') {
-			return data.first + ' ' + data.last;
+function getNameFromData(data) {
+	if (typeof data === "object") {
+		if (typeof data.first === "string" && typeof data.last === "string") {
+			return data.first + " " + data.last;
 		} else if (data.id) {
 			return data.id;
 		}
@@ -39,7 +37,7 @@ function getNameFromData (data) {
 	return data;
 }
 
-function smoothScrollTop () {
+function smoothScrollTop() {
 	var position = window.scrollY || window.pageYOffset;
 	var speed = position / 10;
 
@@ -54,40 +52,32 @@ function smoothScrollTop () {
 }
 
 var EditForm = React.createClass({
-	displayName: 'EditForm',
+	displayName: "EditForm",
 	propTypes: {
 		data: React.PropTypes.object,
 		list: React.PropTypes.object,
 	},
-	getInitialState () {
-		var hasAsyncFields = !!this.props.list.columns.find(col => {
-			if (col.field && col.field.type === 'relationship') {
-				var fieldData = this.props.data.fields[col.field.path];
-				return col.field.many ? fieldData.length > 0 : fieldData;
-			} else {
-				return false;
-			}
-		});
+	getInitialState() {
 		return {
 			values: assign({}, this.props.data.fields),
 			confirmationDialog: null,
-			loading: hasAsyncFields,
-			hasLoaded: !hasAsyncFields,
+			loading: false,
 			lastValues: null, // used for resetting
-			focusFirstField: !this.props.list.nameField && !this.props.list.nameFieldIsFormHeader,
+			focusFirstField:
+				!this.props.list.nameField && !this.props.list.nameFieldIsFormHeader,
 		};
 	},
-	componentDidMount () {
+	componentDidMount() {
 		this.__isMounted = true;
 	},
-	componentWillUnmount () {
+	componentWillUnmount() {
 		this.__isMounted = false;
 	},
-	getFieldProps (field) {
+	getFieldProps(field) {
 		const props = assign({}, field);
 		const alerts = this.state.alerts;
 		// Display validation errors inline
-		if (alerts && alerts.error && alerts.error.error === 'validation errors') {
+		if (alerts && alerts.error && alerts.error.error === "validation errors") {
 			if (alerts.error.detail[field.path]) {
 				// NOTE: This won't work yet, as ElementalUI doesn't allow
 				// passed in isValid, only invalidates via internal state.
@@ -95,86 +85,74 @@ var EditForm = React.createClass({
 				props.isValid = false;
 			}
 		}
-		props.value = this.state.values[field.path] === undefined ? field.defaultValue : this.state.values[field.path];
+		props.value =
+			this.state.values[field.path] === undefined
+				? field.defaultValue
+				: this.state.values[field.path];
 		props.values = this.state.values;
 		props.onChange = this.handleChange;
-		props.mode = 'edit';
-
-		// add a callback on RelationshipField element props to call when values are fully loaded
-		if (props.type === 'relationship' && !this.state.hasLoaded) {
-			if (props.many && props.value.length > 0 || !props.many && !!props.value) {
-				this.registerAsyncField(field.path);
-				props.onValuesLoaded = this.onAsyncFieldValuesLoaded;
-			}
-		}
+		props.mode = "edit";
 		return props;
 	},
-
-	registerAsyncField (fieldName) {
-		this.__asyncFields = this.__asyncFields || {};
-		this.__asyncFields[fieldName] = this.__asyncFields[fieldName] || ASYNC_FIELD_LOADING;
-	},
-
-	onAsyncFieldValuesLoaded (fieldName) {
-		this.__asyncFields[fieldName] = ASYNC_FIELD_LOADED;
-		var isLoadingComplete = Object.values(this.__asyncFields).filter(asyncStatus => asyncStatus !== ASYNC_FIELD_LOADED).length === 0;
-		this.setState({
-			loading: !isLoadingComplete,
-			hasLoaded: isLoadingComplete
-		});
-	},
-
-	handleChange (event) {
+	handleChange(event) {
 		const values = assign({}, this.state.values);
 
 		values[event.path] = event.value;
 		this.setState({ values });
 	},
 
-	toggleDeleteDialog () {
+	toggleDeleteDialog() {
 		this.setState({
 			deleteDialogIsOpen: !this.state.deleteDialogIsOpen,
 		});
 	},
-	toggleResetDialog () {
+	toggleResetDialog() {
 		this.setState({
 			resetDialogIsOpen: !this.state.resetDialogIsOpen,
 		});
 	},
-	handleReset () {
+	handleReset() {
 		this.setState({
 			values: assign({}, this.state.lastValues || this.props.data.fields),
 			resetDialogIsOpen: false,
 		});
 	},
-	handleDelete () {
+	handleDelete() {
 		const { data } = this.props;
 		this.props.dispatch(deleteItem(data.id, this.props.router));
 	},
-	handleKeyFocus () {
+	handleKeyFocus() {
 		const input = this.refs.keyOrIdInput;
 		input.select();
 	},
-	removeConfirmationDialog () {
+	removeConfirmationDialog() {
 		this.setState({
 			confirmationDialog: null,
 		});
 	},
-	updateItem () {
+	updateItem() {
 		const { data, list } = this.props;
 		const editForm = this.refs.editForm;
 
 		// Fix for Safari where XHR form submission fails when input[type=file] is empty
 		// https://stackoverflow.com/questions/49614091/safari-11-1-ajax-xhr-form-submission-fails-when-inputtype-file-is-empty
-		$(editForm).find("input[type='file']").each(function () {
-			if ($(this).get(0).files.length === 0) { $(this).prop('disabled', true); }
-		});
+		$(editForm)
+			.find("input[type='file']")
+			.each(function () {
+				if ($(this).get(0).files.length === 0) {
+					$(this).prop("disabled", true);
+				}
+			});
 
 		const formData = new FormData(editForm);
 
-		$(editForm).find("input[type='file']").each(function () {
-			if ($(this).get(0).files.length === 0) { $(this).prop('disabled', false); }
-		});
+		$(editForm)
+			.find("input[type='file']")
+			.each(function () {
+				if ($(this).get(0).files.length === 0) {
+					$(this).prop("disabled", false);
+				}
+			});
 
 		// Show loading indicator
 		this.setState({
@@ -196,7 +174,7 @@ var EditForm = React.createClass({
 				this.setState({
 					alerts: {
 						success: {
-							success: 'Your changes have been saved successfully',
+							success: "Your changes have been saved successfully",
 						},
 					},
 					lastValues: this.state.values,
@@ -206,8 +184,8 @@ var EditForm = React.createClass({
 			}
 		});
 	},
-	renderKeyOrId () {
-		var className = 'EditForm__key-or-id';
+	renderKeyOrId() {
+		var className = "EditForm__key-or-id";
 		var list = this.props.list;
 
 		if (list.nameField && list.autokey && this.props.data[list.autokey.path]) {
@@ -217,20 +195,46 @@ var EditForm = React.createClass({
 						modified="ID:"
 						normal={`${upcase(list.autokey.path)}: `}
 						title="Press <alt> to reveal the ID"
-						className="EditForm__key-or-id__label" />
+						className="EditForm__key-or-id__label"
+					/>
 					<AltText
-						modified={<input ref="keyOrIdInput" onFocus={this.handleKeyFocus} value={this.props.data.id} className="EditForm__key-or-id__input" readOnly />}
-						normal={<input ref="keyOrIdInput" onFocus={this.handleKeyFocus} value={this.props.data[list.autokey.path]} className="EditForm__key-or-id__input" readOnly />}
+						modified={
+							<input
+								ref="keyOrIdInput"
+								onFocus={this.handleKeyFocus}
+								value={this.props.data.id}
+								className="EditForm__key-or-id__input"
+								readOnly
+							/>
+						}
+						normal={
+							<input
+								ref="keyOrIdInput"
+								onFocus={this.handleKeyFocus}
+								value={this.props.data[list.autokey.path]}
+								className="EditForm__key-or-id__input"
+								readOnly
+							/>
+						}
 						title="Press <alt> to reveal the ID"
-						className="EditForm__key-or-id__field" />
+						className="EditForm__key-or-id__field"
+					/>
 				</div>
 			);
 		} else if (list.autokey && this.props.data[list.autokey.path]) {
 			return (
 				<div className={className}>
-					<span className="EditForm__key-or-id__label">{list.autokey.path}: </span>
+					<span className="EditForm__key-or-id__label">
+						{list.autokey.path}:{" "}
+					</span>
 					<div className="EditForm__key-or-id__field">
-						<input ref="keyOrIdInput" onFocus={this.handleKeyFocus} value={this.props.data[list.autokey.path]} className="EditForm__key-or-id__input" readOnly />
+						<input
+							ref="keyOrIdInput"
+							onFocus={this.handleKeyFocus}
+							value={this.props.data[list.autokey.path]}
+							className="EditForm__key-or-id__input"
+							readOnly
+						/>
 					</div>
 				</div>
 			);
@@ -239,63 +243,70 @@ var EditForm = React.createClass({
 				<div className={className}>
 					<span className="EditForm__key-or-id__label">ID: </span>
 					<div className="EditForm__key-or-id__field">
-						<input ref="keyOrIdInput" onFocus={this.handleKeyFocus} value={this.props.data.id} className="EditForm__key-or-id__input" readOnly />
+						<input
+							ref="keyOrIdInput"
+							onFocus={this.handleKeyFocus}
+							value={this.props.data.id}
+							className="EditForm__key-or-id__input"
+							readOnly
+						/>
 					</div>
 				</div>
 			);
 		}
 	},
-	renderNameField () {
+	renderNameField() {
 		var nameField = this.props.list.nameField;
 		var nameFieldIsFormHeader = this.props.list.nameFieldIsFormHeader;
-		var wrapNameField = field => (
-			<div className="EditForm__name-field">
-				{field}
-			</div>
+		var wrapNameField = (field) => (
+			<div className="EditForm__name-field">{field}</div>
 		);
 		if (nameFieldIsFormHeader) {
 			var nameFieldProps = this.getFieldProps(nameField);
 			nameFieldProps.label = null;
-			nameFieldProps.size = 'full';
+			nameFieldProps.size = "full";
 			nameFieldProps.autoFocus = true;
 			nameFieldProps.inputProps = {
-				className: 'item-name-field',
+				className: "item-name-field",
 				placeholder: nameField.label,
-				size: 'large',
+				size: "large",
 			};
 			return wrapNameField(
 				React.createElement(Fields[nameField.type], nameFieldProps)
 			);
 		} else {
-			return wrapNameField(
-				<h2>{this.props.data.name || '(no name)'}</h2>
-			);
+			return wrapNameField(<h2>{this.props.data.name || "(no name)"}</h2>);
 		}
 	},
-	renderFormElements () {
+	renderFormElements() {
 		var headings = 0;
 
 		return this.props.list.uiElements.map((el, index) => {
 			// Don't render the name field if it is the header since it'll be rendered in BIG above
 			// the list. (see renderNameField method, this is the reverse check of the one it does)
 			if (
-				this.props.list.nameField
-				&& el.field === this.props.list.nameField.path
-				&& this.props.list.nameFieldIsFormHeader
-			) return;
+				this.props.list.nameField &&
+				el.field === this.props.list.nameField.path &&
+				this.props.list.nameFieldIsFormHeader
+			)
+				return;
 
-			if (el.type === 'heading') {
+			if (el.type === "heading") {
 				headings++;
 				el.options.values = this.state.values;
-				el.key = 'h-' + headings;
+				el.key = "h-" + headings;
 				return React.createElement(FormHeading, el);
 			}
 
-			if (el.type === 'field') {
+			if (el.type === "field") {
 				var field = this.props.list.fields[el.field];
 				var props = this.getFieldProps(field);
-				if (typeof Fields[field.type] !== 'function') {
-					return React.createElement(InvalidFieldType, { type: field.type, path: field.path, key: field.path });
+				if (typeof Fields[field.type] !== "function") {
+					return React.createElement(InvalidFieldType, {
+						type: field.type,
+						path: field.path,
+						key: field.path,
+					});
 				}
 				props.key = field.path;
 				if (index === 0 && this.state.focusFirstField) {
@@ -305,17 +316,13 @@ var EditForm = React.createClass({
 			}
 		}, this);
 	},
-	renderFooterBar () {
+	renderFooterBar() {
 		if (this.props.list.noedit && this.props.list.nodelete) {
 			return null;
 		}
 
-		const { loading, hasLoaded } = this.state;
-		const loadingButtonText = loading
-			? hasLoaded
-				? 'Saving'
-				: 'Loading'
-			: 'Save';
+		const { loading } = this.state;
+		const loadingButtonText = loading ? "Saving" : "Save";
 
 		// Padding must be applied inline so the FooterBar can determine its
 		// innerHeight at runtime. Aphrodite's styling comes later...
@@ -335,15 +342,25 @@ var EditForm = React.createClass({
 						</LoadingButton>
 					)}
 					{!this.props.list.noedit && (
-						<Button disabled={loading} onClick={this.toggleResetDialog} variant="link" color="cancel" data-button="reset">
-							<ResponsiveText
-								hiddenXS="reset changes"
-								visibleXS="reset"
-							/>
+						<Button
+							disabled={loading}
+							onClick={this.toggleResetDialog}
+							variant="link"
+							color="cancel"
+							data-button="reset"
+						>
+							<ResponsiveText hiddenXS="reset changes" visibleXS="reset" />
 						</Button>
 					)}
 					{!this.props.list.nodelete && (
-						<Button disabled={loading} onClick={this.toggleDeleteDialog} variant="link" color="delete" style={styles.deleteButton} data-button="delete">
+						<Button
+							disabled={loading}
+							onClick={this.toggleDeleteDialog}
+							variant="link"
+							color="delete"
+							style={styles.deleteButton}
+							data-button="delete"
+						>
 							<ResponsiveText
 								hiddenXS={`delete ${this.props.list.singular.toLowerCase()}`}
 								visibleXS="delete"
@@ -354,7 +371,7 @@ var EditForm = React.createClass({
 			</FooterBar>
 		);
 	},
-	renderTrackingMeta () {
+	renderTrackingMeta() {
 		// TODO: These fields are visible now, so we don't want this. We may revisit
 		// it when we have more granular control over hiding fields in certain
 		// contexts, so I'm leaving this code here as a reference for now - JW
@@ -366,24 +383,33 @@ var EditForm = React.createClass({
 		var data = {};
 
 		if (this.props.list.tracking.createdAt) {
-			data.createdAt = this.props.data.fields[this.props.list.tracking.createdAt];
+			data.createdAt =
+				this.props.data.fields[this.props.list.tracking.createdAt];
 			if (data.createdAt) {
 				elements.push(
 					<FormField key="createdAt" label="Created on">
-						<FormInput noedit title={moment(data.createdAt).format('DD/MM/YYYY h:mm:ssa')}>{moment(data.createdAt).format('Do MMM YYYY')}</FormInput>
+						<FormInput
+							noedit
+							title={moment(data.createdAt).format("DD/MM/YYYY h:mm:ssa")}
+						>
+							{moment(data.createdAt).format("Do MMM YYYY")}
+						</FormInput>
 					</FormField>
 				);
 			}
 		}
 
 		if (this.props.list.tracking.createdBy) {
-			data.createdBy = this.props.data.fields[this.props.list.tracking.createdBy];
+			data.createdBy =
+				this.props.data.fields[this.props.list.tracking.createdBy];
 			if (data.createdBy && data.createdBy.name) {
 				let createdByName = getNameFromData(data.createdBy.name);
 				if (createdByName) {
 					elements.push(
 						<FormField key="createdBy" label="Created by">
-							<FormInput noedit>{data.createdBy.name.first} {data.createdBy.name.last}</FormInput>
+							<FormInput noedit>
+								{data.createdBy.name.first} {data.createdBy.name.last}
+							</FormInput>
 						</FormField>
 					);
 				}
@@ -391,24 +417,36 @@ var EditForm = React.createClass({
 		}
 
 		if (this.props.list.tracking.updatedAt) {
-			data.updatedAt = this.props.data.fields[this.props.list.tracking.updatedAt];
-			if (data.updatedAt && (!data.createdAt || data.createdAt !== data.updatedAt)) {
+			data.updatedAt =
+				this.props.data.fields[this.props.list.tracking.updatedAt];
+			if (
+				data.updatedAt &&
+				(!data.createdAt || data.createdAt !== data.updatedAt)
+			) {
 				elements.push(
 					<FormField key="updatedAt" label="Updated on">
-						<FormInput noedit title={moment(data.updatedAt).format('DD/MM/YYYY h:mm:ssa')}>{moment(data.updatedAt).format('Do MMM YYYY')}</FormInput>
+						<FormInput
+							noedit
+							title={moment(data.updatedAt).format("DD/MM/YYYY h:mm:ssa")}
+						>
+							{moment(data.updatedAt).format("Do MMM YYYY")}
+						</FormInput>
 					</FormField>
 				);
 			}
 		}
 
 		if (this.props.list.tracking.updatedBy) {
-			data.updatedBy = this.props.data.fields[this.props.list.tracking.updatedBy];
+			data.updatedBy =
+				this.props.data.fields[this.props.list.tracking.updatedBy];
 			if (data.updatedBy && data.updatedBy.name) {
 				let updatedByName = getNameFromData(data.updatedBy.name);
 				if (updatedByName) {
 					elements.push(
 						<FormField key="updatedBy" label="Updated by">
-							<FormInput noedit>{data.updatedBy.name.first} {data.updatedBy.name.last}</FormInput>
+							<FormInput noedit>
+								{data.updatedBy.name.first} {data.updatedBy.name.last}
+							</FormInput>
 						</FormField>
 					);
 				}
@@ -422,10 +460,12 @@ var EditForm = React.createClass({
 			</div>
 		) : null;
 	},
-	render () {
+	render() {
 		return (
 			<form ref="editForm" className="EditForm-container">
-				{(this.state.alerts) ? <AlertMessages alerts={this.state.alerts} /> : null}
+				{this.state.alerts ? (
+					<AlertMessages alerts={this.state.alerts} />
+				) : null}
 				<Grid.Row>
 					<Grid.Col large="three-quarters">
 						<Form layout="horizontal" component="div">
@@ -435,7 +475,9 @@ var EditForm = React.createClass({
 							{this.renderTrackingMeta()}
 						</Form>
 					</Grid.Col>
-					<Grid.Col large="one-quarter"><span /></Grid.Col>
+					<Grid.Col large="one-quarter">
+						<span />
+					</Grid.Col>
 				</Grid.Row>
 				{this.renderFooterBar()}
 				<ConfirmationDialog
@@ -444,7 +486,9 @@ var EditForm = React.createClass({
 					onCancel={this.toggleResetDialog}
 					onConfirmation={this.handleReset}
 				>
-					<p>Reset your changes to <strong>{this.props.data.name}</strong>?</p>
+					<p>
+						Reset your changes to <strong>{this.props.data.name}</strong>?
+					</p>
 				</ConfirmationDialog>
 				<ConfirmationDialog
 					confirmationLabel="Delete"
@@ -452,7 +496,8 @@ var EditForm = React.createClass({
 					onCancel={this.toggleDeleteDialog}
 					onConfirmation={this.handleDelete}
 				>
-					Are you sure you want to delete <strong>{this.props.data.name}?</strong>
+					Are you sure you want to delete{" "}
+					<strong>{this.props.data.name}?</strong>
 					<br />
 					<br />
 					This cannot be undone.
@@ -465,7 +510,7 @@ var EditForm = React.createClass({
 const styles = {
 	footerbar: {
 		backgroundColor: fade(theme.color.body, 93),
-		boxShadow: '0 -2px 0 rgba(0, 0, 0, 0.1)',
+		boxShadow: "0 -2px 0 rgba(0, 0, 0, 0.1)",
 		paddingBottom: 20,
 		paddingTop: 20,
 		zIndex: 99,
@@ -474,7 +519,7 @@ const styles = {
 		height: theme.component.height, // FIXME aphrodite bug
 	},
 	deleteButton: {
-		float: 'right',
+		float: "right",
 	},
 };
 
